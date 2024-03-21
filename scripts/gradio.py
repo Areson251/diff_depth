d089932320d3953146fe51515c33ca991b6caebc
Areson251 <unicorn5596@gmail.com>
2024-03-21 15:56:13 +0300

    output to logs

--- scripts/gradio.py
+++ scripts/gradio.py
@@ -6,31 +6,56 @@ import gradio as gr
 from .stable_diffusion import StableDiffusionModel
 from .kandinsky import KandinskyModel
 import logging
+import time 
 
 class GradioWindow():
     def __init__(self) -> None:
         self.path_to_orig_imgs = "images/orig_imgs"
-        self.path_to_output_imgs = "images/output_imgs"
+        self.path_to_output_imgs = "images/output_imgs/exp2"
         self.path_to_prompts = "images/prompts.txt"
         self.path_to_negative_prompts = "images/negative_prompts.txt"
+        self.path_to_logs = "out.log"
 
         self.original_img = None
         self.masks = None
         self.prompts = None
+        self.logger = None
+        self.stable_diffusion = None 
+        self.kandinsky = None 
+
+        #TODO: rewrite time calculating to something goodx
+        self.sd_avg_time = 0
+        self.kandinsky_avg_time = 0
 
         self.folders = [self.path_to_orig_imgs, self.path_to_output_imgs]
 
-        # self.stable_diffusion = StableDiffusionModel()
-        self.kandinsky = KandinskyModel()
-
         self.check_folders()
+        self.start_logging()
         self.read_prompts()
+        self.load_models()
+
         self.main()
+
+    def start_logging(self):
+        self.logger = logging.getLogger(__name__)
+        path = os.path.join(self.path_to_output_imgs, self.path_to_logs)
+        logging.basicConfig(filename=path, level=logging.INFO)
+        self.logger.info("Started")
 
     def check_folders(self):
         for path in self.folders:
             if not os.path.exists(path):
                 os.makedirs(path)
+
+    def read_prompts(self):
+        with open(self.path_to_prompts, "r") as file:
+            self.prompts = [line.rstrip() for line in file]
+            self.logger.info("Use prompts from "+self.path_to_prompts)
+
+    def load_models(self):
+        self.stable_diffusion = StableDiffusionModel()
+        self.kandinsky = KandinskyModel()
+        self.logger.info("Models loaded")
 
     def main(self):
         with gr.Blocks() as self.demo:
@@ -76,52 +101,72 @@ class GradioWindow():
         mask = input_img["layers"][0]
         mask = np.array(Image.fromarray(np.uint8(mask)).convert("L"))
         self.masks = np.where(mask != 0, 255, 0)
+        self.logger.info("New mask has been drawn")
         return [self.original_img, self.masks, input_img["composite"]]
     
     def prepare_input(self, image, mask):
+        self.logger.info("Image shape: "+str(np.array(image).shape))
+        self.logger.info("Mask shape: "+str(np.array(mask).shape))
         print(np.array(image).shape, np.array(mask).shape)
+
         image = Image.fromarray(np.uint8(image)).convert("RGB")
         w_orig, h_orig = image.size
         image = image.resize((512, 512))
         mask = Image.fromarray(np.uint8(mask)).resize((512, 512), Image.NEAREST)
-        print(np.unique(np.array(mask)))
+
         print(np.array(image).shape, np.array(mask).shape)
+        self.logger.info("New image shape: "+str(np.array(image).shape))
+        self.logger.info("New mask shape: "+str(np.array(mask).shape))
         return image, mask, w_orig, h_orig
 
     def inpaint_image(self, iter_number, guidance_scale):
         image, mask, w_orig, h_orig = self.prepare_input(self.original_img, self.masks)
 
         for prompt in self.prompts:
-            # try:
-            #     # TODO: write common AutoPipelineForInpainting for all models
-            #     # TODO: use some bib for logs
-            #     self.stable_diffusion_image = self.stable_diffusion.diffusion_inpaint(
-            #         image, mask, prompt, None, w_orig, h_orig, 
-            #         iter_number, guidance_scale,
-            #     )
-            #     self.save_img(self.stable_diffusion_image, "SDXL_"+prompt)
-            # except Exception as error:
-            #     print("ERROR WITH GENERATING IMAGE VIA SDXL: ", error)
+            try:
+                self.logger.info("Generate SD with prompt: "+prompt)
+                # TODO: write common AutoPipelineForInpainting for all models
+                # TODO: use some bib for logs
+                start_time = time.time()
+                self.stable_diffusion_image = self.stable_diffusion.diffusion_inpaint(
+                    image, mask, prompt, None, w_orig, h_orig, 
+                    iter_number, guidance_scale,
+                )
+                curr_time = time.time()
+                self.sd_avg_time += curr_time-start_time
+                self.logger.info("SDXL generated time: "+str(curr_time-start_time))
+                self.save_img(self.stable_diffusion_image, "SDXL_"+prompt)
+            except Exception as error:
+                self.logger.info("ERROR WITH GENERATING IMAGE VIA SDXL: "+error)
+                print("ERROR WITH GENERATING IMAGE VIA SDXL: ", error)
 
             try:
+                self.logger.info("Generate KAND with prompt: "+prompt)
+                start_time = time.time()
                 self.kandinsky_image = self.kandinsky.diffusion_inpaint(
                     image, mask, prompt, None, w_orig, h_orig, 
                     iter_number, guidance_scale,
                 )
+                curr_time = time.time()
+                self.kandinsky_avg_time += curr_time-start_time
+                self.logger.info("SDXL generated time: "+str(curr_time-start_time))
                 self.save_img(self.kandinsky_image, "KAND_"+prompt)
             except Exception as error:
+                self.logger.info("ERROR WITH GENERATING IMAGE VIA KANDINSKY: "+error)
                 print("ERROR WITH GENERATING IMAGE VIA KANDINSKY: ", error)
 
+        self.logger.info("DONE GENERATING IMAGES")
+        self.logger.info("AVERAGE TIME FOR MODELS:")
+        self.logger.info("Stable Diffusion: "+str(self.sd_avg_time/len(self.prompts)))
+        self.logger.info("Kandinsky: "+str(self.kandinsky_avg_time/len(self.prompts)))
         return self.kandinsky_image, self.stable_diffusion_image
-    
-    def read_prompts(self):
-        with open(self.path_to_prompts, 'r') as file:
-            self.prompts = [line.rstrip() for line in file]
 
     def save_img(self, img, prompt):
         im = Image.fromarray(img)
-        im.save(os.path.join(self.path_to_output_imgs, prompt) + ".png")
-        print("SAVED: ", prompt)
+        path = os.path.join(self.path_to_output_imgs, prompt) + ".png"
+        im.save(path)
+        self.logger.info("Save image to "+path)
+        print("SAVED: ", path)
     
 
 window = GradioWindow()
